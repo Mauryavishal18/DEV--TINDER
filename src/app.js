1c0dff7c62dbf077c0767ae9c603bd8718a79351
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-01-29 21:49:47 +0530

    Various Request Handling

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ const express=require('express');
 
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send('Hello From Test Route');
+// app.use("/test",(req,res)=>{
+//     res.send('Hello From Test Route');
+// });
+
+// app.use("/hello",(req,res)=>{
+//     res.send('Hello World');
+// });
+
+app.get('/status',(req,res)=>{
+    res.send('Server is running');
 });
 
-app.use("/hello",(req,res)=>{
-    res.send('Hello World');
+app.post('/data',(req,res)=>{
+    res.send('Data received');
 });
 
+
+
+app.delete('/delete',(req,res)=>{
+    res.send('Delete route');
+});
 
 
 
