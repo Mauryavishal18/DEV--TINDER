87714866fcba43c078144e21ee3c211b72d994dd
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-02-01 23:13:34 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ const express=require('express');
 const connectDB=require("./config/database");
 const app=express();
 
+// const userSchema=require("./models/user");
+const User=require("./models/user");
 
 
+app.post("/signup",async (req,res)=>{
+    const user=new User({
+        firstName:"Rohit",
+        lastName:"Sharma",
+        emailId:"rohit.sharma@example.com",
+        age:37,
+        gender:"Male",
+    });
+    try{
+    await user.save();
+    res.send("User Added successfully!");
+
+
+
+
+}catch(err){
+    res.status(400).send("Error signing up user: " + err.message);
+}
+
+});
 
 
 
